6b08f17190b2f2b11a44a5d6094edd5abe6608b1
Anirudh-Palakkal <anirudh.n.palakkal@gmail.com>
2022-06-30 14:40:42 -0400

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         // Say hello to the world!
+        // another comment
         System.out.println("Hello");
     }
 }
