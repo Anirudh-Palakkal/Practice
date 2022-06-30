1c1a4f67e3f42faca868b01126e2d77ded16bc0f
AnirudhPalakkal <palakkal@sheridancollege.ca>
2022-06-30 14:46:05 -0400

    First commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         // Say hello to the world!
         // another comment
+        // third comment
         System.out.println("Hello");
     }
 }
