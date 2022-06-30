7d87c776c8a124bf012074b1cff657b231ebc016
AnirudhPalakkal <palakkal@sheridancollege.ca>
2022-06-30 15:12:35 -0400

    Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // Say hello to the world!
-        // another comment
-        // third comment
         System.out.println("Hello");
     }
 }
